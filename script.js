152c2a588666823b2100077a7d60a704f89d04e6
Ajay giri <ajaygiri9099@gmail.com>
2023-04-09 15:16:54 -0400

    Oploading loops

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict'
+
+//CODE CHALLENGE
+//LOOPS
+
+// 1.
+
+for (let i = 1; i <= 10; i++){
+    console.log(i)
+    
+}
+
+// 2.
+
+for (let i = 0; i <= 20; i++){
+    console.log(i)
+}
+
+// 3.
+
+let num1 = 0;
+let num2 = 1;
+let fib;
+
+console.log(num1);
+console.log(num2);
+
+for (let i = 3; i <= 20; i++) {
+  fib = num1 + num2;
+  console.log(fib);
+  num1 = num2;
+  num2 = fib;
+}
+
+// 4.
+
+for(let i =1 ; i <= 10; i = i +1 )
+console.log('7*', i, '=', i * 7)
+
+
+// QUESTION 2.
+
+
+
